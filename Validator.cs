a0ad29e20f95d05896394fc4b4976e8499e374e1
Silio Leonardo (CtP/ETS) <sii5ct@bosch.com>
2023-03-23 14:42:36 -0300

    Add graph and geometric object to Validator

--- Validator.cs
+++ Validator.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 public class Validator
 {
     private IEnumerable<Domino> dominoes;
+    private DominoGraph graph;
+    private GeometricGraph geo;
 
-    public Validator(IEnumerable<Domino> dominoes)
+    public Validator(
+        IEnumerable<Domino> dominoes,
+        DominoGraph graph,
+        GeometricGraph geo
+    )
     {
         this.dominoes = dominoes;
+        this.graph = graph;
+        this.geo = geo;
     }
+
+    
 }
